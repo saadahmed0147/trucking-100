0edb25a330efafa8a32aa8765d3d4f867c44e83d
saad <saadahmed0147@gmail.com>
2025-07-22 22:56:58 +0500

    added api key

---
+++ lib/api_keys.dart
@@ -0,0 +1,5 @@
+class ApiKeys {
+  static const String googleMapsApiKey =
+      'AIzaSyDo8HGqkDwHuSuxcWAkHuK7H_gv1ThasBg';
+  static const String geminiApiKey = 'AIzaSyCUB1tKpdqjPl5_GgHmc0b4rRbpV_TtjRc';
+}
